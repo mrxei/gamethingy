f0ab5f969eb2c919492887ed2f0c2708c7ca0e4c
xei <161970778+mrxei@users.noreply.github.com>
2025-02-13 20:48:03 +0000

    Co-authored-by: Sriman <srimannreddy@users.noreply.github.com>

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -11,8 +11,21 @@ export class Game extends Scene
         super('Game');
     }
 
+    preload ()
+    {
+        this.load.image('sky', 'assets/sky.png');
+        this.load.image('ground', 'assets/platform.png');
+        this.load.image('star', 'assets/star.png');
+        this.load.image('bomb', 'assets/bomb.png');
+        this.load.spritesheet('dude', 
+            'assets/dude.png',
+            { frameWidth: 32, frameHeight: 48 }
+        );
+    }
+
     create ()
     {
+        this.add.image(512,384,'sky');
         this.camera = this.cameras.main;
         this.camera.setBackgroundColor(0x00ff00);
 
